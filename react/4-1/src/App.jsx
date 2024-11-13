3676767aa5ff03f6e7579679a490d17b00726aef
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-14 00:52:17 +0100

    Setup

---
+++ react/4-1/src/App.jsx
@@ -0,0 +1,7 @@
+import { Tabs } from "./Tabs/Tabs";
+
+export const App = () => (
+  <Tabs>
+    <h1>HTTP Requests</h1>
+  </Tabs>
+);
---
+++ react/4-2/src/App.jsx
@@ -0,0 +1,7 @@
+import { Tabs } from "./Tabs/Tabs";
+
+export const App = () => (
+  <Tabs>
+    <h1>Custom Hooks</h1>
+  </Tabs>
+);
