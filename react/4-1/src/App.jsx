43b261b6760c5ade0dc8ab7c52d2595513937167
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-14 04:23:03 +0100

    React Module 4 Part 1 - before

--- react/4-1/src/App.jsx
+++ react/4-1/src/App.jsx
@@ -1,7 +1,14 @@
 import { Tabs } from "./Tabs/Tabs";
+import { FetchingData } from "./0-fetching-data/FetchingData";
+import { LoadingState } from "./1-loading-state/LoadingState";
+import { ErrorState } from "./2-error-state/ErrorState";
+import { ApiDecoupling } from "./3-api-decoupling/ApiDecoupling";
 
 export const App = () => (
   <Tabs>
-    <h1>HTTP Requests</h1>
+    <FetchingData />
+    <LoadingState />
+    <ErrorState />
+    <ApiDecoupling />
   </Tabs>
 );
