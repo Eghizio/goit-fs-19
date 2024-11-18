8e538ad28fad22cc18573606313d279382273f04
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-18 02:28:07 +0100

    Setup

---
+++ react/5-1/src/App.jsx
@@ -0,0 +1,7 @@
+import { Tabs } from "./Tabs/Tabs";
+
+export const App = () => (
+  <Tabs>
+    <h1>Client Side Routing</h1>
+  </Tabs>
+);
---
+++ react/5-2/src/App.jsx
@@ -0,0 +1,7 @@
+import { Tabs } from "./Tabs/Tabs";
+
+export const App = () => (
+  <Tabs>
+    <h1>Navigation & URL Params</h1>
+  </Tabs>
+);
