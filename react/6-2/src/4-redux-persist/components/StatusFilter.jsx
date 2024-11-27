e50867525ebfead19ac62821738701059c70dc4d
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-27 06:32:26 +0100

    React Module 6 Part 2 - before - Added Redux Persist

---
+++ react/6-2/src/4-redux-persist/components/StatusFilter.jsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { FilterButton } from "./FilterButton";
+import { getFilterStatus } from "../redux/selectors/filter";
+import { setStatusFilter } from "../redux/slices/filter";
+import { STATUS_FILTERS } from "../constants/filter";
+
+export const StatusFilter = () => {
+  const filter = useSelector(getFilterStatus);
+  const dispatch = useDispatch();
+
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
+
+  return (
+    <div>
+      <FilterButton
+        selected={filter === STATUS_FILTERS.ALL}
+        onClick={() => handleFilterChange(STATUS_FILTERS.ALL)}
+      >
+        All
+      </FilterButton>
+
+      <FilterButton
+        selected={filter === STATUS_FILTERS.ACTIVE}
+        onClick={() => handleFilterChange(STATUS_FILTERS.ACTIVE)}
+      >
+        Active
+      </FilterButton>
+
+      <FilterButton
+        selected={filter === STATUS_FILTERS.COMPLETED}
+        onClick={() => handleFilterChange(STATUS_FILTERS.COMPLETED)}
+      >
+        Completed
+      </FilterButton>
+    </div>
+  );
+};
--- react/6-2/src/App.jsx
+++ react/6-2/src/App.jsx
@@ -3,6 +3,7 @@ import { ReduxToolkitStore } from "./0-redux-toolkit-store/ReduxToolkitStore";
 import { ActionCreators } from "./1-action-creators/ActionCreators";
 import { ReducerCreators } from "./2-reducer-creators/ReducerCreators";
 import { Slices } from "./3-slices/Slices";
+import { ReduxPersist } from "./4-redux-persist/ReduxPersist";
 
 export const App = () => (
   <Tabs>
@@ -10,5 +11,6 @@ export const App = () => (
     <ActionCreators />
     <ReducerCreators />
     <Slices />
+    <ReduxPersist />
   </Tabs>
 );
