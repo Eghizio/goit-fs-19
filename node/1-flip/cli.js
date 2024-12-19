b63b0aa1e1f767c208e7cbea210aedc408ac1a96
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-12-19 18:57:40 +0100

    Setup

---
+++ node/1-flip/cli.js
@@ -0,0 +1,9 @@
+/*
+Task: { "id": "5", "text": "Hello from Node.js 👋", "completed": false }
+- Get all tasks.
+- Get task by id or completed-status (true/false)
+- Search by text (could add Fuzzy search - fuze.js)
+- Add new task
+- Overwrite task (update)
+- Delete task
+*/
