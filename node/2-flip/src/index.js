d288c38386eecff4e0c231b329827b02682ab7b0
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-01-02 11:04:23 +0100

    Flip description adjustment

--- node/2-flip/src/index.js
+++ node/2-flip/src/index.js
@@ -1,6 +1,10 @@
 /*
-Create Tasks API.
-With minimal Frontend.
-Save to file.
-Try to use the previous code for operating on tasks within the filesystem.
+    - Create Tasks API with filesystem storage.
+    - Try to use the previous code for operating on tasks within the filesystem.
+    - Test using Postman or any other HTTP client.
+
+    - [Optional]: Create minimal Frontend for the application.
+    
+    - [Challenge]: Add ENV variable API_KEY with some random value.
+    Secure the endpoints to require passing the API_KEY in request Headers.
 */
