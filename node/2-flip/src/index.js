73924d042db889c0b73d1a2e87c50243e6730139
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-01-02 11:10:14 +0100

    Added README for Flipped

--- node/2-flip/src/index.js
+++ node/2-flip/src/index.js
@@ -1,9 +1,15 @@
 /*
+    # Tasks REST API
     - Create Tasks API with filesystem storage.
+    
     - Try to use the previous code for operating on tasks within the filesystem.
+    
     - Test using Postman or any other HTTP client.
 
+    # Extras
     - [Optional]: Create minimal Frontend for the application.
+
+    - [Experiment]: Use a different file format for storing tasks (e.g. CSV).
     
     - [Challenge]: Add ENV variable API_KEY with some random value.
     Secure the endpoints to require passing the API_KEY in request Headers.
