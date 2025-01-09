88331b808c41226f67f5e90de8f7724ba0d4156c
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-01-09 19:02:18 +0100

    Node Module 3 Part 2 - before

---
+++ node/3-2/src/SimpleStudent.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+
+const simpleStudentsSchema = new Schema({
+  name: String,
+  age: Number,
+  grades: [Number],
+  isAdmin: Boolean,
+});
+
+export const SimpleStudent = mongoose.model(
+  "simplestudent",
+  simpleStudentsSchema
+);
